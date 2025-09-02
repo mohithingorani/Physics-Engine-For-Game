f3951bee55a492765ef17d1eb04cca7214f685da
Mohit <mohithingorani2003@gmail.com>
2025-09-02 09:32:22 +0000

    listening to events for player 1 on websocket

--- server/dist/types/messages.js
+++ server/dist/types/messages.js
@@ -1,10 +1,15 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
-exports.movementTypes = exports.messageTypes = void 0;
+exports.movementTypes = exports.playerTypes = exports.messageTypes = void 0;
 var messageTypes;
 (function (messageTypes) {
     messageTypes["MOVEMENT"] = "movement";
 })(messageTypes || (exports.messageTypes = messageTypes = {}));
+var playerTypes;
+(function (playerTypes) {
+    playerTypes["PLAYER1"] = "player1";
+    playerTypes["PLAYER2"] = "player2";
+})(playerTypes || (exports.playerTypes = playerTypes = {}));
 var movementTypes;
 (function (movementTypes) {
     movementTypes["LEFT"] = "left";
--- server/src/types/messages.ts
+++ server/src/types/messages.ts
@@ -1,10 +1,16 @@
 export type message = {
     type : messageTypes,
-    move : movementTypes
+    move : movementTypes,
+    player : playerTypes
 }
 
 export enum messageTypes{
     MOVEMENT = "movement"
+}
+
+export enum playerTypes {
+    PLAYER1 = "player1",
+    PLAYER2 = "player2"
 }
 
 export enum movementTypes{
